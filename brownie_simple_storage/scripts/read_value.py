a37cf548c17c86841ea840186bb068c3784acbfa
Rana-usman-ul-haq <98952692+Rana-usman-ul-haq@users.noreply.github.com>
2022-06-30 17:06:35 +0500

    Delete brownie_simple_storage directory

--- brownie_simple_storage/scripts/read_value.py
+++
@@ -1,9 +0,0 @@
-from brownie import SimpleStorage, accounts, config
-
-
-def read_contract():
-    simple_storage = SimpleStorage[-1]
-    #take index one less than length
-    #ABI
-    #address
-    print(simple_storage.retrieve())
